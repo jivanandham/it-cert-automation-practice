e10484578fca1a4abf27942edc5f170d21eda106
JEEVA KRISHNASAMY <jeevakrishnasamy@macbookair.mynetworksettings.com>
2025-02-13 17:35:05 -0500

    validate_user function behaviour updated

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -4,7 +4,7 @@ import re
 
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
-    if type(username) != str:
+    if not isinstance(username, str):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
@@ -13,12 +13,9 @@ def validate_user(username, minlen):
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9._]*$', username):
+    if not re.fullmatch(r'[A-Za-z0-9._]+', username):
         return False
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
     return True
-
-
-
